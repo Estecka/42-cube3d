0b9e7958c1d5d22f6b2f58dd079ffda728757ebd
Estecka <estecka@galacsys.com>
2020-01-15 15:27:40 +0100

    Imported get_next_line into libft.

---
+++ libft/get_next_line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/04 16:20:24 by abaur             #+#    #+#             */
+/*   Updated: 2020/01/07 13:16:42 by abaur            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+typedef struct s_gnlbuffer	t_gnlbuffer;
+struct	s_gnlbuffer {
+	t_gnlbuffer	*next;
+	size_t		size;
+	char		content[BUFFER_SIZE];
+};
+
+int		get_next_char(int fd, char *dst);
+int		get_next_line(int fd, char **line);
+
+#endif
