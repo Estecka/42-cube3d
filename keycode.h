5903d2a6e1e10391a4e6e8b07dd9a2237d098af0
Adrien Baur <abaur@student.42.fr>
2020-01-27 15:17:50 +0100

    Added Keycode.h

---
+++ keycode.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keycode.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/27 15:12:41 by abaur             #+#    #+#             */
+/*   Updated: 2020/01/27 15:17:37 by abaur            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYCODE_H
+# define KEYCODE_H
+
+# define KCESC	53
+
+# define KCW	13
+# define KCA	 0
+# define KCS	 1
+# define KCD	 2
+
+# define KCLEFT		123
+# define KCRIGHT	124
+# define KCDOWN		125
+# define KCUP		126
+
+# define KCSPACE	49
+
+#endif
