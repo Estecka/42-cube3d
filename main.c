d22ab60ffed326d1e6a7ad2d3b1a293e51d96918
Adrien Baur <abaur@student.42.fr>
2020-01-28 15:05:56 +0100

    Made a few global variables.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 15:38:42 by abaur             #+#    #+#             */
-/*   Updated: 2020/01/28 12:52:06 by abaur            ###   ########.fr       */
+/*   Updated: 2020/01/28 15:03:13 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 
 #include "minilibx/mlx.h"
 #include "keycode.h"
+
+void		*g_mlx = NULL;
+void		*g_window = NULL;
 
 static int	exitonx(void *null)
 {
@@ -41,8 +44,6 @@ extern int	main(int argc, char **args)
 {
 	int			fd;
 	t_mapfile	map;
-	void		*mlx;
-	void		*win;
 
 	if (argc < 2)
 		throw(-1, "Not enough arguments.");
@@ -51,10 +52,10 @@ extern int	main(int argc, char **args)
 		throw(errno, "Could not open file: %d", errno);
 	ft_bzero(&map, sizeof(t_mapfile));
 	parsefile(fd, &map);
-	if (!(mlx = mlx_init()))
+	if (!(g_mlx = mlx_init()))
 		throw(errno, "[FATAL] MinilibX init failed : %d", errno);
-	win = mlx_new_window(mlx, map.screenwdt, map.screenhgt, "Cube3D : The Reckoning");
-	mlx_key_hook(win, exitonesc, NULL);
-	mlx_hook(win, 17, 1<<17, exitonx, NULL);
-	mlx_loop(mlx);
+	g_window = mlx_new_window(g_mlx, map.screenwdt, map.screenhgt, "Cube3D : The Reckoning");
+	mlx_key_hook(g_window, exitonesc, NULL);
+	mlx_hook(g_window, 17, 1<<17, exitonx, NULL);
+	mlx_loop(g_mlx);
 }
