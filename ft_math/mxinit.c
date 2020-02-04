bf1f5c5ee046f69d6631c05ffde38f8672f8d7a7
Adrien Baur <abaur@student.42.fr>
2020-02-04 16:30:25 +0100

    Updated ft_math.

--- ft_math/mxinit.c
+++ ft_math/mxinit.c
@@ -1,39 +1,58 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mxdet.c                                            :+:      :+:    :+:   */
+/*   mxinit.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 12:26:03 by abaur             #+#    #+#             */
-/*   Updated: 2020/02/03 11:51:34 by abaur            ###   ########.fr       */
+/*   Updated: 2020/01/31 12:43:40 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "matrix.h"
 
 /*
-** Computes a 2x2 matrix's determinant.
-** @param const t_mx2 m The matrix whose determinant to compute.
-** @return float The determinant.
+** Initializes a matrix to the identity.
+** @param t_mx matrix The matrix to initialize.
 */
 
-extern float	mx2det(const t_mx2 m)
+extern void mx2init(t_mx2 m)
 {
-	return ((m[0][0] * m[1][1]) - (m[0][1] * m[1][0]));
+	m[0][0] = 1;
+	m[0][1] = 0;
+	m[1][0] = 0;
+	m[1][1] = 1;
 }
 
-/*
-** Computes a 3x3 matrix's determinant.
-** @param const t_mx3 m The matrix whose determinant to compute.
-** @return float The determinant.
-*/
-
-extern float	mx3det(const t_mx3 m)
+extern void	mx3init(t_mx3 m)
 {
-	return (
-		m[0][0] * ((m[1][1] * m[2][2]) - (m[1][2] * m[2][1]))
-		- m[1][0] * ((m[0][1] * m[2][2]) - (m[0][2] * m[2][1]))
-		+ m[2][0] * ((m[0][1] * m[1][2]) - (m[0][2] * m[1][1]))
-);
+	m[0][0] = 1;
+	m[0][1] = 0;
+	m[0][2] = 0;
+	m[1][0] = 0;
+	m[1][1] = 1;
+	m[1][2] = 0;
+	m[2][0] = 0;
+	m[2][1] = 0;
+	m[2][2] = 1;
+}
+extern void	mx4init(t_mx4 m)
+{
+	m[0][0] = 1;
+	m[0][1] = 0;
+	m[0][2] = 0;
+	m[0][3] = 0;
+	m[1][0] = 0;
+	m[1][1] = 1;
+	m[1][2] = 0;
+	m[1][3] = 0;
+	m[2][0] = 0;
+	m[2][1] = 0;
+	m[2][2] = 1;
+	m[2][3] = 0;
+	m[3][0] = 0;
+	m[3][1] = 0;
+	m[3][2] = 0;
+	m[3][3] = 1;
 }
