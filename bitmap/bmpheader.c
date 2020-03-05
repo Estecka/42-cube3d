1625aae7275899ce82cbaa06643c61fb745619b3
Adrien Baur <abaur@student.42.fr>
2020-03-05 16:42:39 +0100

    Added bmp_validate_headers.

--- bitmap/bmpheader.c
+++ bitmap/bmpheader.c
@@ -42,3 +42,16 @@ static short	get_bmp_headers(int fd, t_bmpheader *header, t_bmpinfo *info)
 			return (0);
 	return (1);
 }
+
+static short	bmp_validate_headers(t_bmpheader *header, t_bmpinfo *info)
+{
+	if (header->filetype[0] != 'B' || header->filetype[1] != 'M'
+		|| header->pixeldataoffset != sizeof(t_bmpheader) + info->headersize
+		|| info->imagewidth <= 0 || info->imagewidth <= 0
+		|| info->planes != 1
+		|| (info->bitsperpixel != 32 && info->bitsperpixel != 24)
+		|| info->totalcolors != 0 || info->importantcolors != 0)
+		return (0);
+	else
+		return (1);
+}
