7e4d73a0816184ce31cae8ec5d7ac0b0aa2c08f1
abaur <abaur@student.42.fr>
2020-04-29 19:51:58 +0200

    Fixed bmp_write.

--- bitmap/bmpwrite.c
+++ bitmap/bmpwrite.c
@@ -14,13 +14,19 @@
 
 #include "bmp_internals.h"
 
+/*
+** @return bool
+** 	true  OK
+** 	false error
+*/
+
 static short	bmp_write_header(t_mlx_img *this, int fd)
 {
 	t_bmpheader	header;
 
-	header = (t_bmpheader){ 0 };
+	header = (t_bmpheader){};
 	header.filetype[0] = 'B';
-	header.filetype[0] = 'M';
+	header.filetype[1] = 'M';
 	header.pixeldataoffset = sizeof(t_bmpheader) + sizeof(t_bmpinfo);
 	header.filesize = header.pixeldataoffset;
 	header.filesize += this->width * this->height * 32;
@@ -29,6 +35,12 @@ static short	bmp_write_header(t_mlx_img *this, int fd)
 	else
 		return (1);
 }
+
+/*
+** @return bool
+** 	true  OK
+** 	false error
+*/
 
 static short	bmp_write_info(t_mlx_img *this, int fd)
 {
@@ -49,6 +61,12 @@ static short	bmp_write_info(t_mlx_img *this, int fd)
 		return (1);
 }
 
+/*
+** @return bool
+** 	true  OK
+** 	false error
+*/
+
 static short	bmp_write_pixel(t_mlx_img *src, int fd)
 {
 	unsigned int	x;
@@ -60,9 +78,9 @@ static short	bmp_write_pixel(t_mlx_img *src, int fd)
 	{
 		x = -1;
 		while (++x < src->width)
-			if (0 > write(fd, mlx_img_getptr(src, x, src->height - 1 - y), 24))
+			if (0 > write(fd, mlx_img_getptr(src, x, src->height - 1 - y), 3))
 				return (0);
-		size = (this->width * 24) % 32;
+		size = (src->width * 24) % 32;
 		if (size && 0 > write(fd, "\0\0\0\0", size))
 			return (0);
 	}
@@ -73,7 +91,8 @@ extern short	bmp_write(t_mlx_img *this, const char *path)
 {
 	int	fd;
 
-	if (0 >	fd  = open(path, O_CREAT | O_WRONLY | O_TRUNC))
+	if (0 >	(fd = open(path, O_CREAT | O_WRONLY | O_TRUNC,
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)))
 		return (0);
 	if (!bmp_write_header(this, fd)
 		|| !bmp_write_info(this, fd)
